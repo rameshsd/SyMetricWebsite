14abaa8a849473eb3e9bdd780165b8c9c38444d2
Ramesh Dodamani <rameshdodamanisites@gmail.com>
2025-10-23 10:04:47 +0000

    Try fixing this error: `Runtime Error: Error: Invalid src prop (https://

--- next.config.ts
+++ next.config.ts
@@ -34,6 +34,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'cgv.c32.myftpupload.com',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
 };
