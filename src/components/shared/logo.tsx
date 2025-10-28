13c2d01bf60ca0d7950b9df2a29f7da84d6d8bb6
Ramesh Dodamani <rameshdodamanisites@gmail.com>
2025-10-28 07:50:36 +0000

    This is the logo and Include the same

--- src/components/shared/logo.tsx
+++ src/components/shared/logo.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 export function Logo({ className }: { className?: string }) {
   return (
     <Link href="/" className="flex items-center gap-2 font-bold text-lg text-foreground">
-      <Image src="/logo.svg" alt="SyMetric Logo" width={32} height={32} className='w-8 h-8' />
+      <Image src="https://symetricsystems.com/wp-content/uploads/2021/05/logo_1-300x79.png" alt="SyMetric Logo" width={120} height={32} />
     </Link>
   )
 }
