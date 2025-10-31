5fef85b4f203b924e6b9713111b4fb5866ae26e7
Ramesh Dodamani <rameshdodamanisites@gmail.com>
2025-10-31 07:49:05 +0000

    Services and Sub Smenu services Link Is not working

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -26,11 +26,12 @@ import { Input } from '../ui/input';
 const ListItem = React.forwardRef<
   React.ElementRef<"a">,
   React.ComponentPropsWithoutRef<"a">
->(({ className, title, children, ...props }, ref) => {
+>(({ className, title, children, href, ...props }, ref) => {
   return (
     <li>
       <NavigationMenuLink asChild>
-        <a
+        <Link
+          href={href || '#'}
           ref={ref}
           className={cn(
             "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:text-primary focus:text-primary",
@@ -42,7 +43,7 @@ const ListItem = React.forwardRef<
           <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
             {children}
           </p>
-        </a>
+        </Link>
       </NavigationMenuLink>
     </li>
   )
