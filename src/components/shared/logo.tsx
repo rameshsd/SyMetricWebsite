468ad4a60abb5dfc9b477955bef2f2ec0ae1640a
Ramesh Dodamani <rameshdodamanisites@gmail.com>
2025-10-31 07:41:06 +0000

    https://symetricsystems.com/wp-content/uploads/2021/05/symetric.png
    Log

--- src/components/shared/logo.tsx
+++ src/components/shared/logo.tsx
@@ -3,12 +3,12 @@ import Image from 'next/image';
 import { PlaceHolderImages } from '@/lib/placeholder-images';
 
 export function Logo({ className }: { className?: string }) {
-  const logoImage = PlaceHolderImages.find(p => p.id === 'sap-logo');
+  const logoImage = PlaceHolderImages.find(p => p.id === 'symetric-logo');
   
   return (
     <a href="/" className="flex items-center gap-2 font-bold text-lg text-foreground">
       {logoImage ? (
-        <Image src={logoImage.imageUrl} alt="SAP Logo" width={60} height={30} />
+        <Image src={logoImage.imageUrl} alt="SyMetric Logo" width={120} height={30} />
       ) : (
         <span>SyMetric</span>
       )}
