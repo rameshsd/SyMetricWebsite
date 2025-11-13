22b12be6299a664c28c24a4e5f8d7ff4075b9e1d
Ramesh Dodamani <rameshdodamanisites@gmail.com>
2025-11-13 08:09:35 +0000

    Try fixing this error: `Console Error: An empty string ("") was passed t

--- src/components/solutions/ProductHero.tsx
+++ src/components/solutions/ProductHero.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { Button } from '@/components/ui/button';
 import { cn } from '@/lib/utils';
-import { IrtDiagram } from '../animations/IrtDiagram';
+import IrtDiagram from '../animations/IrtDiagram';
 
 type ProductHeroProps = {
     title: string;
