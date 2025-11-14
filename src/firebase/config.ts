17c9891ee0cb938ae3d1eec3f4798b405189e5b9
Ramesh Dodamani <rameshdodamanisites@gmail.com>
2025-11-14 07:10:18 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-441500127-2590b",
+  "appId": "1:606236610750:web:e81dc25435bf0dcbd28eef",
+  "apiKey": "AIzaSyAMCSDgiUIpPHDM3Ns7wm2v6yWmmDYCg-I",
+  "authDomain": "studio-441500127-2590b.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "606236610750"
+};
