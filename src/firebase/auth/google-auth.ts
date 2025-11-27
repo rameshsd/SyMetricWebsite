7f220c23dddfacee9155b3f1a1219cb3d351f3de
Ramesh Dodamani <rameshdodamanisites@gmail.com>
2025-11-27 07:47:33 +0000

    'use client'; import { Auth, GoogleAuthProvider, signInWithPopup } from

--- src/firebase/auth/google-auth.ts
+++ src/firebase/auth/google-auth.ts
@@ -1,5 +1,5 @@
 'use client';
-import { Auth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { auth } from '@/firebase';
 
 const provider = new GoogleAuthProvider();
