b51f9b6f52237c546b441976b584df786c01f0c7
Ramesh Dodamani <rameshdodamanisites@gmail.com>
2025-12-01 18:13:32 +0000

    See you Have given That much of space Please reduce that Space Between E

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -37,7 +37,7 @@ export default function Home() {
   
   return (
     <div className="flex flex-col min-h-[100dvh]">
-      <main className="flex-1 space-y-12 md:space-y-16">
+      <main className="flex-1 space-y-8 md:space-y-10">
          <SapTechedHero />
         <RevolutionizingTrials />
         <UnlockPotential />
