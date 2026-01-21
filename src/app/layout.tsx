458d41ff7a56a9018b0d6f445bef99083cfdc798
Ramesh Dodamani <rameshdodamanisites@gmail.com>
2026-01-21 08:26:25 +0000

    and according to system mode like dark mode and light mode it will chang

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
       <body>
         <ThemeProvider
             attribute="class"
-            defaultTheme="system"
+            defaultTheme="light"
             enableSystem
             disableTransitionOnChange
         >
